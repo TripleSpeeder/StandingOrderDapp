287f3dcf6b45e00eddaa5dc228252e7bac73de72
Michael Bauer <michael@m-bauer.org>
2017-04-30 02:07:19 +0200

    StandingOrderDapp-14 Add description to orders

--- src/RelabelForm.js
+++ src/RelabelForm.js
@@ -1,49 +1,54 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {Button, Form, ControlLabel, FormGroup, FormControl} from 'react-bootstrap'
+
 
 
 class RelabelForm extends Component {
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             label: props.label,
-        };
+        }
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleInputChange(event) {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
+        const target = event.target
+        const name = target.name
+        const value = target.value
         this.setState({
             [name]: value
-        });
+        })
     }
 
     handleSubmit(event) {
-        event.preventDefault();
-        console.log(this.state.label);
+        event.preventDefault()
+        console.log(this.state.label)
         // Notify parent about new Label
-        this.props.onRelabel(this.state.label);
+        this.props.onRelabel(this.state.label)
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Label:
-                    <input
+            <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                    <ControlLabel>Enter new Label:</ControlLabel>
+                    <FormControl
                         name="label"
                         type="text"
                         value={this.state.label}
+                        placeholder="Enter label"
                         onChange={this.handleInputChange}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
-        );
+                </FormGroup>
+                <Button type="submit">
+                    Submit
+                </Button>
+            </Form>
+        )
     }
 }
 
