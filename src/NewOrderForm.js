4a4b100b7da564febadd79537d28717702160a1a
Michael Bauer <michael@m-bauer.org>
2017-05-25 15:31:28 +0200

    Fix warning about formcontrol changing from uncontrolled to controlled by setting default value for contract label.

--- src/NewOrderForm.js
+++ src/NewOrderForm.js
@@ -13,7 +13,7 @@ class NewOrderForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            label: props.label,
+            label: '',
             period: 60 * 60 * 24 * 7, // 1 week
             rate: window.web3.toBigNumber('0'),
             startTime: moment()
