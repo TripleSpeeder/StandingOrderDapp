a6a9b8ad69d5d63a387d6ab2e75f3dcc6c98b546
michael.bauer <michael@m-bauer.org>
2017-05-28 14:55:18 +0200

    Display dummy funding result data when transaction details could not be retrieved. This seems only to happen with testrpc running with -b.

--- src/FundOrderResultModal.js
+++ src/FundOrderResultModal.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Modal, Button} from 'react-bootstrap'
+import {Modal, Button, Alert} from 'react-bootstrap'
 import EtherDisplay from "./EtherDisplay"
 
 
@@ -8,7 +8,22 @@ class FundOrderResultModal extends Component {
 
     render() {
         if (this.props.transaction===null) {
-            return <div></div>
+            return <Modal bsSize="small" show={this.props.showModal} onHide={this.props.onClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Funding successfull</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Alert bsStyle="warning">
+                        <strong>Got empty transaction result. This is most likely a bug in testRPC when running with -b option (background mining).</strong>
+                    </Alert>
+                    <p>Sent XXX eth.</p>
+                    <p><a href="#" target="_blank">Verify on etherchain.org</a></p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.props.onClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+
         }
 
         let verifyUrl = 'https://etherchain.org/tx/' + this.props.transaction.hash
@@ -33,8 +48,6 @@ class FundOrderResultModal extends Component {
 }
 
 FundOrderResultModal.propTypes = {
-    //order: PropTypes.object.isRequired,
-    //onWithdraw: PropTypes.func.isRequired,
     showModal: PropTypes.bool.isRequired,
     onClose: PropTypes.func.isRequired,
 }
