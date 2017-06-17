96e4d4353e9852507f3c06746bd220f39f62f5e4
Michael Bauer <michael@m-bauer.org>
2017-06-17 02:04:19 +0200

    split and cleanup testcases.

--- test/TestFundedStandingOrder.js
+++ test/TestFundedStandingOrder.js
@@ -13,60 +13,26 @@ let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 var StandingOrder = artifacts.require('StandingOrder')
 var StandingOrderFactory = artifacts.require('StandingOrderFactory')
 
-web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
-    var transactionReceiptAsync
-    interval = interval ? interval : 500
-    transactionReceiptAsync = function (txnHash, resolve, reject) {
-        web3.eth.getTransactionReceipt(txnHash, (error, receipt) => {
-            if (error) {
-                reject(error)
-            } else {
-                if (receipt == null) {
-                    setTimeout(function () {
-                        transactionReceiptAsync(txnHash, resolve, reject)
-                    }, interval)
-                } else {
-                    resolve(receipt)
-                }
-            }
-        })
-    }
-
-    if (Array.isArray(txnHash)) {
-        var promises = []
-        txnHash.forEach(function (oneTxHash) {
-            promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval))
-        })
-        return Promise.all(promises)
-    } else {
-        return new Promise(function (resolve, reject) {
-            transactionReceiptAsync(txnHash, resolve, reject)
-        })
-    }
-}
-
 contract('StandingOrderFactory', function (accounts) {
 
     let order
     let owner = accounts[0]
     let payee = accounts[1]
-    let paymentAmount = web3.toWei(1, 'finney')
-    let fundAmount = web3.toWei(10, 'finney')
+    let otherUser = accounts[2]
+    let paymentAmount = web3.toWei(1, 'ether')
+    let fundAmount = web3.toWei(10, 'ether')
 
+    // TODO: Don't use factory here, instead just manually create an order instance!
     before('Create a standingOrder', function () {
         let interval = 60 // one minute
         let startTime = moment() // First payment due now
         let label = 'testorder'
-
 
         return StandingOrderFactory.deployed().then(function (factory) {
             return factory.createStandingOrder(payee, paymentAmount, interval, startTime.unix(), label, {
                 from: owner,
                 gas: 1000000
             }).then(function (result) {
-                // now wait till transaction is mined, only then the contract is deployed.
-                web3.eth.getTransactionReceiptMined(result.tx)
-            }).then(function () {
                 // now get the actual standingOrderContract
                 return factory.getOwnOrderByIndex.call(0, {from: owner})
             }).then(function (address) {
@@ -81,14 +47,13 @@ contract('StandingOrderFactory', function (accounts) {
     before('Fund order', function () {
         return order.send(fundAmount, {from: owner})
             .then(function (result) {
-                // now wait till transaction is mined
-                web3.eth.getTransactionReceiptMined(result.tx)
-            }).then(function () {
-                // done!
+                assert.isNotNull(result.receipt.blockHash)
+                assert(web3.eth.getBalance(order.address).equals(fundAmount))
+                console.log("Order funded!")
         })
     })
 
-    it('should have correct entitledfunds', function () {
+    it('should have correct entitledfunds for payee', function () {
         return order.getEntitledFunds({from: payee}).then(function (entitledFunds) {
             assert(entitledFunds.equals(paymentAmount), 'entitledFunds should match paymentAmount!')
         })
@@ -102,7 +67,8 @@ contract('StandingOrderFactory', function (accounts) {
 
     it('should have funds available for owner withdraw', function () {
         return order.getOwnerFunds({from: owner}).then(function (ownerBalance) {
-            assert(ownerBalance.equals(web3.toBigNumber(fundAmount).minus(paymentAmount)), 'ownerBalance should match fundAmount - paymentAmount!')
+            assert(ownerBalance.equals(web3.toBigNumber(fundAmount).minus(paymentAmount)),
+                'ownerBalance should match fundAmount - paymentAmount!')
         })
     })
 
@@ -113,36 +79,29 @@ contract('StandingOrderFactory', function (accounts) {
         )
     })
 
-    it('should throw when payee calls WithdrawOwnerFunds', function () {
+    it('should throw when non-payee calls collectFunds', function () {
         return assert.isRejected(
-            order.WithdrawOwnerFunds({from: payee}),
+            order.collectFunds({from: otherUser}),
             /invalid opcode/
         )
     })
 
-    xit('should correctly collect funds for payee', function () {
-        let currentBalance
-        let promiseA = web3.eth.getBalance(payee).then(function (balance) {
-            currentBalance = balance
-        })
-        assert.isFulfilled(promiseA)
-        // get current payee balance
-        // call CollectFunds()
-        // again get payee balance
-        // balance diff should match entitled amount.
+    it('should throw when non-owner calls WithdrawOwnerFunds', function () {
+        return assert.isRejected(
+            order.WithdrawOwnerFunds({from: otherUser}),
+            /invalid opcode/
+        )
     })
 
+    // This test will only work if there are unclaimed funds in the contract. Otherwise the contract will immediately
+    // selfdestruct!
     it('should be terminated after calling "withdrawAndTerminate', function () {
         // First verify order is not terminated
         assert.becomes(order.isTerminated({from: owner}), false, 'Contract already terminated before starting test!')
 
         return order.WithdrawAndTerminate({from: owner})
             .then(function (result) {
-                // now wait till transaction is mined, only then the contract is deployed.
-                web3.eth.getTransactionReceiptMined(result.tx)
-            })
-            .then(function () {
-                // contract should be terminated now
+                // contract should be terminated now, but still existing
                 assert.becomes(order.isTerminated({from: owner}), true, 'Contract should now be terminated')
             })
     })
