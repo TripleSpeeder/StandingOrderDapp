bd574024f7aa5fd620d522cbd6581a9348b8d83a
Michael Bauer <michael@m-bauer.org>
2017-07-15 10:38:39 +0200

    Handle failing order creation

--- src/NewOrderButton.js
+++ src/NewOrderButton.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Button, Modal} from 'react-bootstrap'
 import NewOrderForm from './NewOrderForm'
+import CreateOrderResultModal from "./CreateOrderResultModal"
 
 class NewOrderButton extends Component {
 
@@ -9,12 +10,15 @@ class NewOrderButton extends Component {
         super(props)
         this.state = {
             showCreateModal: false,
+            showResultModal: false,
             createOrderProgress: 'idle'
         }
 
         this.onCreateOrder = this.onCreateOrder.bind(this)
         this.openCreateModal = this.openCreateModal.bind(this)
         this.closeCreateModal = this.closeCreateModal.bind(this)
+        this.openResultModal = this.openResultModal.bind(this)
+        this.closeResultModal = this.closeResultModal.bind(this)
     }
 
     onCreateOrder(order) {
@@ -24,10 +28,20 @@ class NewOrderButton extends Component {
             from: this.props.account,
             gas: 1000000
         }).then(function(result){
-            self.setState({createOrderProgress:'done'})
-            self.closeCreateModal()
             console.log('Created StandingOrder - transaction: ' + result.tx)
             console.log(result.receipt)
+            // check for log entries. If there are no logs created, contract creation failed!
+            if(result.logs.length < 1) {
+                throw 'Transaction completed, but no log entries -> Contract creation failed.'
+            }
+            self.setState({createOrderProgress:'done'})
+            self.closeCreateModal()
+        }).catch(function(e) {
+            // There was an error! Handle it.
+            console.log("Error while creating order. Message: " + e)
+            self.setState({createOrderProgress:'idle'})
+            self.closeCreateModal()
+            self.openResultModal()
         })
     }
 
@@ -36,8 +50,21 @@ class NewOrderButton extends Component {
     }
 
     openCreateModal() {
-        this.setState({showCreateModal: true})
+        this.setState(
+            {
+                showCreateModal: true,
+                createOrderProgress:'idle'
+            })
     }
+
+    openResultModal() {
+        this.setState({showResultModal: true})
+    }
+
+    closeResultModal() {
+        this.setState({showResultModal: false})
+    }
+
 
     render() {
         let button = <Button
@@ -59,9 +86,12 @@ class NewOrderButton extends Component {
             </Modal.Body>
         </Modal>
 
+        let resultModal = <CreateOrderResultModal showModal={this.state.showResultModal} onClose={this.closeResultModal}/>
+
         return <div>
             {button}
             {createModal}
+            {resultModal}
         </div>
     }
 }
