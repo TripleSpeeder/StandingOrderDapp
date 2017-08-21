6c2e6bd9937e148ee8f305ce82cab3d4666c5b2f
Michael Bauer <michael@m-bauer.org>
2017-08-21 19:34:47 +0200

    Implement workaround for faulty fromWei() in production build.

--- src/Utils.js
+++ src/Utils.js
@@ -2,7 +2,22 @@ import moment from 'moment'
 import 'moment-duration-format'
 
 export function BigNumWeiToDisplayString(bignum, unit='ether') {
-        return window.web3.fromWei(bignum, unit).toFixed()
+        // Following code does not work in webpack production build:
+        //
+        //  return window.web3.fromWei(bignum, unit).toFixed()
+        //
+        // Contrary to documentation, "fromWei" always returns a string in production build, so
+        // calling <string>.toFixed does not work. In dev build it returns the expected "bignum" instance!
+
+        let x = window.web3.fromWei(bignum, unit)
+        if (typeof(x) === 'string') {
+            // console.log("Expected bigNumber, got String instead: " + x)
+            // console.log("Input was: " + typeof (bignum))
+            return x;
+        } else {
+            // console.log("Got expected bigNumber, returning .toFixed.")
+            return x.toFixed()
+        }
     }
 
 export function secondsToDisplayString(seconds) {
