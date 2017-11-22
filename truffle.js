8b58f648acea86c6bb9c552a120f832c7a513012
Michael Bauer <michael@m-bauer.org>
2017-11-22 11:27:53 +0100

    Remove obsolete "development" network entry from truffle.js. Instead use truffle built-in test network.

--- truffle.js
+++ truffle.js
@@ -1,11 +1,6 @@
 module.exports = {
     migrations_directory: "./migrations",
     networks: {
-        development: {
-            host: "localhost",
-            port: 8546,
-            network_id: "*"
-        },
         live: {
             host: "localhost",
             port: 8545,
